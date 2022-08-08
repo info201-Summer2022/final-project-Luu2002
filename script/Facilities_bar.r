6364fe591c90901ca616edc05fe96b7a12617152
Luu2002 <Lu2002@uw.edu>
2022-08-07 21:09:34 -0700

    add bar chart for facilities dataset

--- script/Facilities_bar.r
+++ script/Facilities_bar.r
@@ -14,11 +14,11 @@ day._treatment <- c(0.445, 3.759, 0.079, 0.101, 0.716, 0.115, 0.163)
 residential_facilities <- c(1.213, 0.366, 0.012, 0.069, 0.076, 0.054)
 mydata <- data.frame(Country, Mental._hospitals, day._treatment, residential_facilities)
 
-# create a scatter plot where xaxis is the country and the yaxis is the rate of each column values
-fig <- plot_ly(mydata, x = ~Country, y = ~Mental._hospitals, type = 'scatter', name = 'Hospitals', mode = 'makers', marker = list(opacity = 1))
-fig <- fig %>% add_trace(y = ~day._treatment, name = 'Daily Treatment')
-fig <- fig %>% add_trace(y = ~residential_facilities, name = 'Redsidential Facilities')
-fig <- fig %>% layout(xaxis = list(showgrid = FALSE), yaxis = list(title = "Rate"), title = "Suicide By Mental Health", barmode = 'group')
+# create a scatter plot where xaxis is the country and the yaxis is the rate of each column values                     
+fig <- plot_ly(mydata, x = ~Country, y = ~Mental._hospitals, type = 'bar', name = "hospitals")
+fig <- fig %>% add_trace(y = ~day._treatment, name = "Daily Treatment")
+fig <- fig %>% add_trace(y = ~residential_facilities, name = "Residential Facilities")
+fig <- fig %>% layout(yaxis = list(title = "rate"),title = 'Suicide By Mental Health', barmode = 'group')
 
 fig
 
