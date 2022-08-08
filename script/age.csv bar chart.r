d5b5b4ade9fb73579fc58c09b4128f162a43ca87
Luu2002 <Lu2002@uw.edu>
2022-08-08 14:14:27 -0700

    edit table summary

--- script/age.csv bar chart.r
+++ script/age.csv bar chart.r
@@ -1,14 +1,15 @@
-
 library(plotly)
-age <- read.csv("~/Desktop/age.csv", header=FALSE)%>%
-  select(V8, V13, V14, V16) %>%
-  filter(V8 %in% c("united states", "Japan", "Mexico, philippines", "Korea", "Poland", "Singapore", "Qatar"))
-V13 <- c(21.6, 16.1, 15.3, 15.3, 8.9, 8.1, 7.9, 3.5)
-V14 <- c(6.5,7.5,9.2,7.6, 4.3, 6.2, 4,1.9)
-V16 <- c(39.2, 25,21.8, 23.2, 13.3, 9.8, 11.8, 5)
-age <- data.frame(V8, V13, V14, V16)
-fig <- plot_ly(age, x = ~V8, y = ~V13, type = "bar", name = "Both")
-fig <- fig %>% add_trace(y = ~V14, name = "Female")
-fig <- fig %>% add_trace(y = ~V16, name = "Male")
-fig <- fig %>% layout(yaxis = list(title = "rate"), title = "Suicide rate By Age", barmode ="group")
+
+suicide_age_df <- read.csv("~/Desktop/age.csv") %>%
+  select(Location, Period, Dim1, Dim2, Dim2ValueCode) %>%
+  filter(Location %in% c("United States of America", "Japan", "Mexico, philippines", "Korea", "Poland", "Singapore", "Qatar"))
+
+Dim2 <- c(15,25,35,45,55,65,76,85)
+suicide_age_df <- df(Location, Period, Dim1, Dim2, Dim2ValueCode)
+
+fig <- plot_ly(age, x = ~Location, y = ~Dim1, type = "bar", name = "Both")
+fig <- fig %>% add_trace(name = "Female")
+fig <- fig %>% add_trace(name = "Male")
+fig <- fig %>% layout(yaxis = list(title = "Age"), title = "Suicide rate By Age", barmode ="group")
+
 fig
--- script/table_sum_age.r
+++ script/table_sum_age.r
@@ -5,15 +5,16 @@ library(dplyr)
 
 # read the data set "Suicide_Age"
 # then filter down the data just to 8 different countries that I selected
-dataset <- read.csv("Suicide_Age.csv") %>%
-  filter(Location %in% c("Japan", "United States of America", "Mexico", "Philippines", "Korea", "Poland", "Singapore", "Qatar"))
+dataset <- read.csv("age.csv") %>%
+  filter(Location %in% c("Japan", "United States of America", "Mexico", "Philippines", "Democratic People's Republic of Korea", "Poland", "Singapore", "Qatar"))
   
 # put the filtered data set into a new table call 'table_summary'
 # then group the data set into 6 different values
 # summaries the value by providing an easy read name
 # then delete unnecessary values from the table
   table_summary <- dataset %>%
-  group_by(Location, Period, Dim1, FactValueNumeric, FactValueNumericLow, FactValueNumericHigh) %>%
-  summarise(Gender = (Dim1), Avg_rate = (FactValueNumeric), Low_rate =(FactValueNumericLow), High_rate = (FactValueNumericHigh))
-  table_summary[, c('Dim1', 'FactValueNumeric', 'FactValueNumericLow', 'FactValueNumericHigh')] <- list(NULL)
+  group_by(Location, Period, Dim1, Dim2, Value, Dim2ValueCode) %>%
+  summarise(Gender = (Dim1), Age_range = (Dim2), Age_code =(Dim2ValueCode))
+  table_summary[, c('Dim1', 'Dim2', 'Dim2ValueCode')] <- list(NULL)
+
   
