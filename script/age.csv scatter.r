fae0f12d74aae9f5ebd471ddceaf73eb402df21d
ZijieLiao <108854993+ZijieLiao@users.noreply.github.com>
2022-08-08 16:42:37 -0700

    Update age.csv scatter.r

--- script/age.csv scatter.r
+++ script/age.csv scatter.r
@@ -1,16 +1,17 @@
 library(plotly)
-age <- read.csv("~/Desktop/age.csv", header=FALSE)%>%
+mydata <- read.csv("~/Desktop/age.csv", header=FALSE)%>%
   select(V8, V24, V27, V30) %>%
   filter(V8 %in% c("united states", "Japan", "Mexico, philippines", "Korea", "Poland", "Singapore", "Qatar"))
-V8 <- c("united states", "Japan", "Mexico", "philippines", "Korea", "Poland", "Singapore", "Qatar")
+
+Country <- c("united states", "Japan", "Mexico", "philippines", "Korea", "Poland", "Singapore", "Qatar")
 V24 <- c(21.6, 16.1, 15.3, 15.3, 8.9, 8.1, 7.9, 3.5)
 V27 <- c(6.5,7.5,9.2,7.6, 4.3, 6.2, 4,1.9)
 V30 <- c(39.2, 25,21.8, 23.2, 13.3, 9.8, 11.8, 5)
-age <- data.frame(V8, V24, V27, V30)
-sca <- plot_ly(age, x = ~V8, y = ~V24, type = 'scatter', name = 'Both', mode = 'makers', marker = list(opacity = 1))
-fig <- fig %>% add_trace(y = ~V27, name = 'High')
-fig <- fig %>% add_trace(y = ~V30, name = 'Low')
-fig <- fig %>% layout(xaxis = list(showgrid = FALSE), yaxis = list(title = "Age"), title = "Average Sucide Age", barmode = 'group')
+mydata <- data.frame(V8, V24, V27, V30)
+
+fig <- plot_ly(mydata, x = ~Country, y = ~V24, type = 'scatter', name = 'Both', mode = 'makers', marker = list(opacity = 1))
+fig <- fig %>% add_trace(y = ~V27, name = 'Male')
+fig <- fig %>% add_trace(y = ~V30, name = 'Female')
+fig <- fig %>% layout(xaxis = list(showgrid = FALSE), yaxis = list(title = "Age"), title = "Average Suicide Age", barmode = 'group')
+
 fig
-
-
